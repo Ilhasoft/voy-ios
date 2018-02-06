f1f180a7687d90943fd9907b99ee3300b486c687
Daniel <daniel@ilhasoft.com.br>
2018-02-06 18:02:07 -0300

    Add icons; Small update

--- Voy/Classes/Controllers/ReportList/Cell/VOYReportTableViewCell.swift
+++ Voy/Classes/Controllers/ReportList/Cell/VOYReportTableViewCell.swift
@@ -35,15 +35,15 @@ class VOYReportTableViewCell: UITableViewCell {
     }
 
     func installShadow(view:UIView) {
-        let radius: CGFloat = (self.contentView.frame.size.width - 60) / 2.0 //change it to .height if you need spread for height
+        let radius: CGFloat = (self.contentView.frame.size.width - 55) / 2.0 //change it to .height if you need spread for height
         let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: 0, width: 2.1 * radius, height: view.frame.height))
         //Change 2.1 to amount of spread you need and for height replace the code for height
         
         view.layer.cornerRadius = 2
         view.layer.shadowColor = UIColor.black.cgColor
         view.layer.shadowOffset = CGSize(width: 0.1, height: 0.2)  //Here you control x and y
-        view.layer.shadowOpacity = 0.1
-        view.layer.shadowRadius = 3.0 //Here your control your blur
+        view.layer.shadowOpacity = 0.25
+        view.layer.shadowRadius = 2.0 //Here your control your blur
         view.layer.masksToBounds =  false
         view.layer.shadowPath = shadowPath.cgPath
     }
