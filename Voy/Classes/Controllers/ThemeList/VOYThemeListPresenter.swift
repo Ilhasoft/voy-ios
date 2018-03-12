f0f33cf0a9941f9d74c5f0edf1b78e13b1bd10fd
Dielson Sales <dielson@ilhasoft.com.br>
2018-03-12 14:29:57 -0300

    Remove unused functions

--- Voy/Classes/Controllers/ThemeList/VOYThemeListPresenter.swift
+++ Voy/Classes/Controllers/ThemeList/VOYThemeListPresenter.swift
@@ -43,7 +43,7 @@ class VOYThemeListPresenter {
     private func cacheData() {
         for project in projects {
             var params = ["project": project.id as Any, "user": VOYUser.activeUser()!.id]
-            dataSource.cacheDataFrom(url: VOYConstant.API.URL + "themes", parameters: &params)
+            dataSource.cacheDataFrom(url: "\(VOYConstant.API.URL)themes", parameters: &params)
         }
     }
 }
