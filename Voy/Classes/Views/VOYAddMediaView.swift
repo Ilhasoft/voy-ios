f80768cdae6122c01de74c5b35f3c951253e739c
Dielson Sales <dielson@ilhasoft.com.br>
2018-03-23 15:33:47 -0300

    Fix ignoring video thumbnail when edit report

--- Voy/Classes/Views/VOYAddMediaView.swift
+++ Voy/Classes/Views/VOYAddMediaView.swift
@@ -19,58 +19,58 @@ class VOYAddMediaView: UIView {
     @IBOutlet weak var imgViewPlusIcon: UIImageView!
     @IBOutlet weak var imgView: UIImageView!
     @IBOutlet var contentView: UIView!
-    
+
     var cameraData: VOYCameraData?
     weak var delegate: VOYAddMediaViewDelegate?
     var media: VOYMedia?
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         initSubviews()
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         initSubviews()
     }
-    
+
     private func initSubviews() {
         let nib = UINib(nibName: "VOYAddMediaView", bundle: Bundle(for: VOYAddMediaView.self))
         nib.instantiate(withOwner: self, options: nil)
         imgView.layer.cornerRadius = 7
         contentView.frame = bounds
-        
+
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
         self.addGestureRecognizer(tapGesture)
         addSubview(contentView)
     }
-    
+
     func setupWithMedia(cameraData: VOYCameraData) {
         self.cameraData = cameraData
         self.imgView.isHidden = false
         self.imgViewPlusIcon.isHidden = true
         self.btRemove.isHidden = false
-        
+
         if let cameraDataImage = cameraData.image, cameraData.type == .image {
             self.imgView.image = cameraDataImage
         } else if let cameraDataThumbnail = cameraData.thumbnail, cameraData.type == .video {
             self.imgView.image = cameraDataThumbnail
         }
     }
-    
+
     func setupWithMedia(media: VOYMedia) {
         self.media = media
         self.imgView.isHidden = false
         self.imgViewPlusIcon.isHidden = true
         self.btRemove.isHidden = false
-        
+
         if media.media_type == VOYMediaType.image.rawValue {
             self.imgView.kf.setImage(with: URL(string: media.file))
-        } else if media.media_type == VOYMediaType.video.rawValue {
-            print("need download video to show thumb")
+        } else if media.media_type == VOYMediaType.video.rawValue, let thumbnail = media.thumbnail {
+            self.imgView.kf.setImage(with: URL(string: thumbnail))
         }
     }
-    
+
     @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
         if let mediaView = gestureRecognizer.view as? VOYAddMediaView {
             self.delegate?.mediaViewDidTap(mediaView: mediaView)
@@ -83,5 +83,5 @@ class VOYAddMediaView: UIView {
         self.imgView.image = nil
         self.imgViewPlusIcon.isHidden = false
     }
-    
+
 }
