ab657d372173bf751b62f072fc5600dd07c1e54f
Dielson Sales <dielson@ilhasoft.com.br>
2018-04-09 14:06:37 -0300

    Implement basic tests for the persistent layer

--- VoyTests/Data/Local/VOYStorageManagerTests.swift
+++ VoyTests/Data/Local/VOYStorageManagerTests.swift
@@ -11,13 +11,69 @@ import XCTest
 
 class VOYStorageManagerTests: XCTestCase {
 
-    let storageManager = VOYDefaultStorageManager()
+    let storageManager: VOYStorageManager = VOYDefaultStorageManager()
 
-    /**
-     * Saves a report and checks if it's actually stored in the pending list.
-     */
-    func testSaveReport() {
-        
+    override func setUp() {
+        super.setUp()
+        storageManager.clearAllOfflineData()
     }
 
+    /**
+     * Saves a report and checks if it's actually stored in the pending list. Then removes it and checks if it has been
+     * actually removed.
+     */
+    func testSaveAndRemoveReport() {
+        let report = VOYReport()
+        report.id = 20
+        report.name = "Test Report"
+
+        // Test save the report
+        storageManager.addPendingReport(report)
+        XCTAssertEqual(storageManager.getPendingReports().count, 1)
+
+        // Checks if the report was stored offline
+        let savedReport = storageManager.getPendingReports().first!
+        let reportId = savedReport["id"] as! Int
+        let reportName = savedReport["name"] as! String
+        XCTAssertEqual(reportId, report.id!)
+        XCTAssertEqual(reportName, report.name!)
+
+        // Overrides the report with a new value
+        report.name = "Changed name"
+        storageManager.addPendingReport(report)
+        XCTAssertEqual(storageManager.getPendingReports().count, 1)
+        let savedReport2 = storageManager.getPendingReports().first!
+        let changedReportName = savedReport2["name"] as! String
+        XCTAssertEqual(reportId, report.id!)
+        XCTAssertEqual(changedReportName, report.name!)
+
+        storageManager.removeFromPendingList(report: report)
+        XCTAssertEqual(storageManager.getPendingReports().count, 0)
+    }
+
+    func testSaveAndRemoveCameraData() {
+        let cameraData = VOYCameraData(
+                image: nil,
+                thumbnail: nil,
+                thumbnailFileName: "thumbnail.jpg",
+                fileName: "image.jpg",
+                type: .image
+        )
+        cameraData.id = "040"
+
+        // Checks if the cameraData was stored offline
+        storageManager.addAsPending(cameraData: cameraData, reportID: 20)
+        XCTAssertEqual(storageManager.getPendingCameraData().count, 1)
+        let savedCameraData = storageManager.getPendingCameraData().first!
+        XCTAssertEqual(savedCameraData["id"] as! String, cameraData.id!)
+        XCTAssertEqual(savedCameraData["path"] as! String, cameraData.fileName!)
+        XCTAssertEqual(savedCameraData["thumbnailPath"] as! String, cameraData.thumbnailFileName!)
+
+        // Overrides the cameraData with a chaged value
+        cameraData.fileName = "image_changed.jpg"
+        storageManager.addAsPending(cameraData: cameraData, reportID: 20)
+        XCTAssertEqual(storageManager.getPendingCameraData().count, 1)
+        let savedCameraData2 = storageManager.getPendingCameraData().first!
+        XCTAssertEqual(savedCameraData2["path"] as! String, cameraData.fileName!)
+    }
 }
