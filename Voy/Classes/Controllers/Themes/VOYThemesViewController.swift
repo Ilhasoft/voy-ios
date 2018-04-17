0b3cabedd3b5a4e129d1f6214a95af6e9e12f83e
Dielson Sales <dielson@ilhasoft.com.br>
2018-04-17 17:48:46 -0300

    Create simpler Themes list

---
+++ Voy/Classes/Controllers/Themes/VOYThemesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VOYThemesViewController.swift
+//  Voy
+//
+//  Created by Dielson Sales on 17/04/18.
+//  Copyright © 2018 Ilhasoft. All rights reserved.
+//
+
+import UIKit
+
+class VOYThemesViewController: UIViewController {
+
+    @IBOutlet weak var lbThemesCount: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
